84137b6b393a2c3203bb1232fa67eb64b89e29e0
Donavfulish <flazerfa123@gmail.com>
2024-08-12 20:58:59 +0700

    UPDATE STRUCT OF NODE AND DATA

---
+++ SubFuntion/Subfunction/Subfunction.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+struct Data
+{
+	string City_Name;
+	float latitude, longtitude;
+};
+
+struct Node
+{
+	Node* leftNode;
+	Node* rightNode;
+	Data key;
+	// Xu ly insert can theo cong thuc deep mod (number of info field) nen mn can phai cap nhat deep moi khi xu ly
+	int deep;
+};
+
+Node* createNode(Data key, int previousDeep)
+{
+	Node* newNode = new Node;
+	newNode->leftNode = NULL;
+	newNode->rightNode = NULL;
+	newNode->key = key;
+	newNode->deep = previousDeep + 1;
+	
+	return newNode;
+}
+Data encryption(string temp)
+{
+	stringstream ss(temp);
+	string word;
+	Data key;
+	int pos = 0;
+
+	while (getline(ss, word, ','))
+	{
+		if (pos == 0)
+			key.City_Name = word;
+
+		else if (pos == 1)
+			key.latitude = stof(word);
+
+		else if (pos == 2)
+			key.longtitude = stof(word);
+
+		else if (pos > 2)
+			break;
+
+		pos++;
+	}
+
+	return key;
+}
+
+vector<Data> readFile(string fileName)
+{
+	fstream fs(fileName.c_str());
+	vector<Data> setof_City;
+	if (!fs.is_open())
+	{
+		cout << "File not found\n";
+		return { };
+	}
+
+	else
+	{
+		string temp;
+		getline(fs, temp);
+
+		while (getline(fs, temp))
+			setof_City.push_back(encryption(temp));
+
+		return setof_City;
+	}
+}
